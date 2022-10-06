b6193ba85cca3bd233b5a3e9699636a58992734c
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2022-10-06 12:00:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,8 @@ streamlit.title ( ' My Parents New Healthy Diner ' )
 streamlit.header ( ' Breakfast Menu ' )
 streamlit.text ( '🥣 Omega 3 & Blueberry Oatmeal ' )
 streamlit.text ( '🥗 Kale , Spinach & Rocket Smoothie ' )
-streamit.text ( '🐔 Hard - Boiled Free - Range Egg ' )
+streamlit.text ( '🐔 Hard - Boiled Free - Range Egg ' )
 streamlit.text ( '🥑🍞 Avocado Toast ' )
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
