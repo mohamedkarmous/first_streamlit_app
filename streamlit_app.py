090ddbdc0f403fab3e2fe463e1c12a3ee995e39e
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2022-10-11 10:23:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 import requests
  
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi" )
  
 
 
