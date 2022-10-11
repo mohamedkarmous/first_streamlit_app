24912542cb0cdfefa97b0a95dcee0dd8343af0f9
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2022-10-11 14:55:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("select * from FRUITYVICE")
 
 my_data_row = my_cur.fetchone()
 
