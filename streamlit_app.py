6be155fe479c338de64c215029cca7784f4efa38
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2022-10-11 10:18:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+ 
+
+streamlit. STREET EMEC Fruit AVEC)
+
+ 
+
 import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
