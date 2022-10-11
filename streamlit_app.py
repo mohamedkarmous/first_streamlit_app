afc4aecefe2f88e59455ed146ff221e07f01d09e
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2022-10-11 15:00:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,6 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUITYVICE")
 my_data_rows = my_cur.fetchall()
 
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_rows)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
